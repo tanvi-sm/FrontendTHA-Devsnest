4a692cb2d59907de299ce38ba1df7f8d7bc47051
tanvi-sm <76934108+tanvi-sm@users.noreply.github.com>
2021-07-12 17:39:28 +0530

    Create script.js

---
+++ DAY - 05/script.js
@@ -0,0 +1,27 @@
+function add() {
+  number1= parseInt(myform.number1.value);
+    number2= parseInt(myform.number2.value);
+    result = number1 + number2;
+    myform.result.value=result;
+}
+
+function sub() {
+  number1= parseInt(myform.number1.value);
+    number2= parseInt(myform.number2.value);
+    result = number1 - number2;
+    myform.result.value=result;
+}
+
+function mul() {
+  number1= parseInt(myform.number1.value);
+    number2= parseInt(myform.number2.value);
+    result = number1 * number2;
+    myform.result.value=result;
+}
+
+function div() {
+  number1= parseInt(myform.number1.value);
+    number2= parseInt(myform.number2.value);
+    result = number1 / number2;
+    myform.result.value=result;
+}
