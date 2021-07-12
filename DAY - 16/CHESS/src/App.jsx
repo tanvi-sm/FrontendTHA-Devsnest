1e7f80b553f2633af5d563110af1d693b631f410
tanvi-sm <76934108+tanvi-sm@users.noreply.github.com>
2021-07-12 18:14:44 +0530

    Create App.jsx

---
+++ DAY - 16/CHESS/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Chess from "./Chess"
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>CHESS GAME</h1>
+     <Chess />
+     {/* <Chess /> */}
+    </div>
+  );
+}
+
+export default App;
