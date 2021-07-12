534b7fc39f5c8b7bddb5ad9f4c7b4d93174e5f17
tanvi-sm <76934108+tanvi-sm@users.noreply.github.com>
2021-07-12 14:27:44 +0530

    Add files via upload

---
+++ DAY 17/src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css';
+import CardsContainer from './CardsContainer';
+
+function App() {
+  return (
+    <div className="App">
+        <CardsContainer />  
+    </div>
+  );
+}
+
+export default App;
