5f6c6dbf9841d8af3eebfcaf7e8ea375cad24f63
tanvi-sm <76934108+tanvi-sm@users.noreply.github.com>
2021-07-12 18:17:30 +0530

    Create App.jsx

---
+++ DAY - 17/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import CardsContainer from './CardsContainer';
+
+function App() {
+  return (
+    <div className="App">
+        <CardsContainer />  
+    </div>
+  );
+}
+
+export default App;
